8d190ab80de9048c9f6826346a047601eb2833d7
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2024-06-11 12:05:07 +0700

    create app bar for "create todo page"

--- lib/src/features/todo/presentation/create_todo_page/create_todo_page.dart
+++ lib/src/features/todo/presentation/create_todo_page/create_todo_page.dart
@@ -1,15 +1,32 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CreateTodoPage extends ConsumerWidget {
   const CreateTodoPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      body: Center(
-        child: AutoSizeText('Create Todo Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: AutoSizeText(
+          'Create New Task',
+          style: GoogleFonts.inter(
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+      ),
+      body: ListView(
+        children: const [
+          
+        ],
       ),
     );
   }
